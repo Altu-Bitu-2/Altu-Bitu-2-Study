d8e464da11c0035caf3cc2a78f6d827948feb97a
jyjyeon <k-j_y@naver.com>
2022-04-04 11:34:26 +0900

    [동적 계획법] 4월 4일 - Update

--- 0329_동적 계획법/9095.cpp
+++ 0329_동적 계획법/9095.cpp
@@ -11,9 +11,18 @@ using namespace std;
 int countSum(int n) {
     vector<int> dp(n + 1, 0);
 
+
+    if (n == 1) {
+        return 1;
+    }
+    else if (n == 2) {
+        return 2;
+    }
+
     dp[1] = 1;
     dp[2] = 2;
     dp[3] = 4;
+
     /*
     ex. 7 = 1 + 6 (= dp[6])
           = 2 + 5 (= dp[5])

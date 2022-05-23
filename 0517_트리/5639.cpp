2e04a62bfa44d57b628eedafc0d99e084a59da23
jyjyeon <k-j_y@naver.com>
2022-05-23 16:14:34 +0900

    [트리] 5월 23일 -ing

--- 0517_트리/5639.cpp
+++ 0517_트리/5639.cpp
@@ -7,16 +7,15 @@
 #include <map>
 
 using namespace std;
-typedef pair<int, int> ci;
-map<int, pair<int, ci>> tree; //pair<int, ci> -> 부모 + 왼쪽, 오른쪽 자식
+map<int, pair<int, int>> tree;
 
 //후위 순회
 void postorder(int v) {
     if (v == NULL) { //자식 없으면
         return;
     }
-    postorder(tree[v].second.first); //왼쪽 자식 출력
-    postorder(tree[v].second.second); //오른쪽 자식 출력
+    postorder(tree[v].first); //왼쪽 자식 출력
+    postorder(tree[v].second); //오른쪽 자식 출력
     cout << v << '\n'; //자기 자신 출력
 }
 
@@ -29,26 +28,24 @@ int main() {
         arr.push_back(node);
     }
 
-    for (int j = 0; j < arr.size() - 1; j++) {
-        if (arr[j] > arr[j + 1]) { //50 30 -> 30은 50의 왼쪽 자식
-            tree[arr[j + 1]].first = arr[j]; //30의 부모: 50
-            tree[arr[j]].second.first = arr[j + 1]; //50의 왼쪽 자식: 30
-            temp = arr[j];
-        }
-        else {
-            root = temp;
-            while (tree[root].second.second) { //오른쪽 자식 붙일 부모 찾기 -> 이미 right child 있으면 안 됨
-                root = tree[root].first;
-            }
-            if (arr[j + 1] > root) { //24 5 28 -> 28 > 24 -> 28은 24의 오른쪽 자식
-                tree[arr[j + 1]].first = root; //28의 부모: 24
-                tree[root].second.second = arr[j + 1]; //24의 오른쪽 자식: 28
-                root--;
-            }
-            else { //98 52 60 -> 60 < 98 -> 60은 98의 자식 X, 52의 오른쪽 자식
-                tree[arr[j + 1]].first = arr[j]; //60의 부모: 52
-                tree[arr[j]].second.second = arr[j + 1]; //52의 오른쪽 자식: 60
+    root = arr[0];
 
+    for (int j = 1; j < arr.size(); j++) {
+        temp = root;
+        while (true) {
+            if (arr[j] < temp) { //삽입할 노드가 루트 노드보다 값이 작을 때
+                if (tree[temp].first == NULL) { //루트 노드에 왼쪽 자식이 비어있을 때
+                    tree[temp].first = arr[j];
+                    break;
+                }
+                temp = tree[temp].first; //다음 루트 -> 왼쪽 자식
+            }
+            else { //삽입할 노드가 루트 노드보다 값이 클 때
+                if (tree[temp].second == NULL) { //루트 노드에 오른쪽 자식이 비어있을 때
+                    tree[temp].second = arr[j];
+                    break;
+                }
+                temp = tree[temp].second; //다음 루트 -> 오른쪽 자식
             }
         }
     }

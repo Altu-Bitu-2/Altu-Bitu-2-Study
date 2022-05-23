cab6a019c098d0f32688a1df8239a8eba3ab48da
jyjyeon <k-j_y@naver.com>
2022-05-23 21:09:49 +0900

    [트리] 5월 23일

--- 0517_트리/5639.cpp
+++ 0517_트리/5639.cpp
@@ -4,18 +4,21 @@
 
 #include <iostream>
 #include <vector>
-#include <map>
 
 using namespace std;
-map<int, pair<int, int>> tree;
+struct node {
+    int left;
+    int right;
+};
+vector<node> tree(10e6);
 
 //후위 순회
 void postorder(int v) {
-    if (v == NULL) { //자식 없으면
+    if (v == 0) { //자식 없으면
         return;
     }
-    postorder(tree[v].first); //왼쪽 자식 출력
-    postorder(tree[v].second); //오른쪽 자식 출력
+    postorder(tree[v].left); //왼쪽 자식 출력
+    postorder(tree[v].right); //오른쪽 자식 출력
     cout << v << '\n'; //자기 자신 출력
 }
 
@@ -32,18 +35,18 @@ int main() {
         root = arr[0];
         while (true) {
             if (arr[j] < root) { //삽입할 노드가 루트 노드보다 값이 작을 때
-                if (tree[root].first == NULL) { //루트 노드에 왼쪽 자식이 비어있을 때
-                    tree[root].first = arr[j];
+                if (tree[root].left == 0) { //루트 노드에 왼쪽 자식이 비어있을 때
+                    tree[root].left = arr[j];
                     break;
                 }
-                root = tree[root].first; //다음 루트 -> 왼쪽 자식
+                root = tree[root].left; //다음 루트 -> 왼쪽 자식
             }
             else { //삽입할 노드가 루트 노드보다 값이 클 때
-                if (tree[root].second == NULL) { //루트 노드에 오른쪽 자식이 비어있을 때
-                    tree[root].second = arr[j];
+                if (tree[root].right == 0) { //루트 노드에 오른쪽 자식이 비어있을 때
+                    tree[root].right = arr[j];
                     break;
                 }
-                root = tree[root].second; //다음 루트 -> 오른쪽 자식
+                root = tree[root].right; //다음 루트 -> 오른쪽 자식
             }
         }
     }
